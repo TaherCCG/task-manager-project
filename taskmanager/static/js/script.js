14e8cdb5189515c54681a0b1bab7c0b35527353c
Taher Mahmood <taher.ccg@gmail.com>
2024-05-18 18:05:08 +0000

    Added Modal to confirm delete in categories.html  and event listener in script.js

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Sidenav Initialisation
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+  
+    let elems = document.querySelectorAll('.modal');
+    M.Modal.init(elems);
   });
