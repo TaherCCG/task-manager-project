a8ee74d16cf4d9ed3a78926d3b92eecf3e344992
Taher Mahmood <taher.ccg@gmail.com>
2024-05-19 21:37:37 +0000

    Update __init__.py with database url

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -7,7 +7,15 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+     uri = os.environ.get("postgres://ualm3umnfkx:Fdz2NohKT6cR@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/mousy_crank_city_24158")
+     if uri.startswith("postgres://"):
+         uri = uri.replace("postgres://", "postgresql://", 1)
+     app.config["SQLALCHEMY_DATABASE_URI"] = uri
+
 
 db = SQLAlchemy(app)
 
