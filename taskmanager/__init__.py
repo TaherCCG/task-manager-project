baf280c2b3771eabe9704580d947ededc67ca159
Taher Mahmood <taher.ccg@gmail.com>
2024-05-19 21:42:54 +0000

    refactor code

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -11,7 +11,7 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
-     uri = os.environ.get("postgres://ualm3umnfkx:Fdz2NohKT6cR@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/mousy_crank_city_24158")
+     uri = os.environ.get("DATABASE_URL")
      if uri.startswith("postgres://"):
          uri = uri.replace("postgres://", "postgresql://", 1)
      app.config["SQLALCHEMY_DATABASE_URI"] = uri
